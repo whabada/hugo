4a629771d8485cf9999f5e6d533b595969af7b75
Ben <Lohrengel.Ben@gmail.com>
2016-02-11 03:11:25 -0800

    Mapper funktioniert

--- src/vicra/ColorAnalyzer.java
+++ src/vicra/ColorAnalyzer.java
@@ -2,7 +2,6 @@ package vicra;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -22,6 +21,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -29,44 +29,49 @@ import org.apache.hadoop.util.ToolRunner;
 public class ColorAnalyzer extends Configured implements Tool {
 
 	private final static IntWritable one = new IntWritable(1);
+	private final static String URL = "UrlString";
 
 	public static class ColorAnalyzerMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
 		private Text rgb = new Text();
+		private String urlStr;
+
+		@Override
+		protected void setup(Context context) {
+			Configuration c = context.getConfiguration();
+			urlStr = c.get("UrlString");
+		} 
 
 		/**
 		 * Die Mapper Funktion erhaelt ein Bild und analysiert die Farbe saemtlicher Pixel des erhaltenen Images. 
 		 * Die Funktion funktioniert nur, wenn das Bild nicht null ist und groeßer als 1x1 ist.
 		 * Für jede Farbe R, G, B wird ausgebenen "RGB Farbcode, 1". 
 		 * @param key Nummer des Frames 
-		 * @param BufferedImage image
+		 * @param Text url
 		 * @param context
 		 * @throws IOException
 		 * @throws InterruptedException
 		 */
 		public void map(LongWritable key, Text url, Context context) throws IOException, InterruptedException {
-			System.out.println("map");
+
+			//TODO das Image aus einer url lesen, die abgegeben wird
 
 			BufferedImage image = null;
-		//	FileSystem fs = FileSystem.get(context.getConfiguration());
-		//	Path inputPath = new Path(url.toString());
+			FileSystem fs = FileSystem.get(context.getConfiguration());
+			Path inputPath = new Path(urlStr);
 			try {
-				 image = ImageIO.read(new File("/home/cloudera/Pictures/sample_01.jpg"));
-				// image = ImageIO.read(new File(url.toString()));
-				//FSDataInputStream in = fs.open(inputPath);
-				//ImageInputStream imageInput = ImageIO.createImageInputStream(in);
-				//image = ImageIO.read(imageInput);
+				FSDataInputStream in = fs.open(inputPath);
+				ImageInputStream imageInput = ImageIO.createImageInputStream(in);
+				image = ImageIO.read(imageInput); 
 
 			} catch (IOException e) {
 			}
-
 
 			if (image != null && image.getWidth() > 1 && image.getHeight() > 1) {
 				// Get dimensions of image
 				int w = image.getWidth();
 				int h = image.getHeight();
 				long sumPixel = w*h;
-				String result ="";
 				long sumr = 0, sumg = 0, sumb = 0;
 				for (int x = 0; x < w; x++) {
 					for (int y = 0; y < h; y++) {
@@ -74,19 +79,21 @@ public class ColorAnalyzer extends Configured implements Tool {
 						sumr = pixel.getRed(); //rot
 						sumg = pixel.getGreen(); // gruen
 						sumb = pixel.getBlue(); // blau
+						//TODO ab hier weiter
+						Text RKey =new Text("R"+blocks(sumr));
+						Text GKey =new Text("G"+blocks(sumg));
+						Text BKey =new Text("B"+blocks(sumb));
+						Text pixelAmount = new Text("SumPixel,"+sumPixel);
 
-						result = sumr + " " +  sumg + " " + sumb;// +" " + sumPixel;
+						/*		String result = RKey.toString() + " " +  GKey.toString() + " " + BKey.toString() + " " + sumPixel;
 						this.rgb.set(result);
-						context.write(rgb, one);
+						context.write(rgb, one); */
+						context.write(RKey, one);
+						context.write(GKey, one);
+						context.write(BKey, one); 
+						context.write(pixelAmount, one);
 					}
 				}
-				//		sumr = sumr / sumPixel;
-				//		sumg = sumg / sumPixel;
-				//		sumb = sumb / sumPixel;
-
-				//TODO Gibt jetzt fuer alle Pixel einen Wert raus
-				//TODO Eingabe in 10er Rangen der Werte
-
 			}
 			else { 
 				System.out.println("kein Bild."); //Kein Bild geladen
@@ -94,84 +101,103 @@ public class ColorAnalyzer extends Configured implements Tool {
 		}
 	}
 
-	public static class AverageColorReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class AverageColorReducer extends Reducer<Text, IntWritable, Text, Text> {
 		private IntWritable sum = new IntWritable();
+
+		/*	  @Override
+          public void setup(Context context) throws IOException, InterruptedException {
+                    mos = new MultipleOutputs<Text, Text>(context);
+          } */
 		/**
 		 * 
 		 * Hier wird gezaehlt, wie häufig ein R G B Code vom Mapper "gesehen" wurde
 		 * Dieses Ergebnis wird aufsummiert und dann als Durchschnittswert ausgegeben 
 		 */
+
 		@Override
-		protected void reduce(Text rgb, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-			/**
+		protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+			String[] splitKey = key.toString().split(",");
+			String code = splitKey[0];
+			
+
 			long sumPixel =0;
+	//		long sumPixel =Long.parseLong(splitKey[1]);;
 			long sumr = 0;
 			long sumg = 0;
 			long sumb = 0;
-			 */
 
-			int total = 0;
-			for (IntWritable val : values) {
-				total += val.get();
-			}
-			sum.set(total);
-			context.write(rgb, sum);
+			//TODO Hier ist ja was nicht richtig: Ich zaehle doch jeden Block, nicht jeden R, G oder B Block. 
 
-			/*	for (IntWritable val : values){
-//TODO Das funktioniert so nicht. Der RGB Wert ist der Key, die Anzahl, wie haeufig ich diesen sehe,
-			 * ist der value. Ich muss die RGB Werte gruppieren GROUPBY, und dann zaehlen - 
-			 * Ich muss den Schluessel auseinander nehmen, nicht den Wert
-				// 
-				// im Text sind die Werte wie folgt angeordnet: sumr + " " +  sumg + " " + sumb +" " + sumPixel;
-
-				String [] result = val.toString().split("\t"); //RGB Werte durch Split wieder auseinander gezogen
-				// RGBwerte werden durch die Anzahl der Pixel geteilt und als Text ausgegeben
-				sumPixel = Long.parseLong(result[3]);
-				sumr += Long.parseLong(result[0]) / sumPixel;
-				sumg += Long.parseLong(result[1]) / sumPixel;
-				sumb += Long.parseLong(result[2]) / sumPixel;			
+			if (code.startsWith("pixelAmount")){
+				sumPixel = Long.parseLong(splitKey[1]);
 			}
 
-			long [] resultArray = new long [3];
-			resultArray[0]= sumr;
-			resultArray[1]= sumg;
-			resultArray[2]= sumb;
-			String stringResult = resultArray [0]+ " " + resultArray [1] + resultArray [2];
-			Text result = new Text(stringResult);
-			context.write(key, result); */
+			
+			if (code.startsWith("R")){
+
+				for (IntWritable val : values) {
+					sumr += val.get();
+				}
+				sumr = sumr / sumPixel;
+			}
+
+			if (code.startsWith("G")){
+				for (IntWritable val : values) {
+					sumg += val.get();
+				}
+				sumg = sumg/sumPixel;
+
+			}
+			if (code.startsWith("B")){
+				for (IntWritable val : values) {
+					sumb += val.get();
+				}
+				sumb = sumb/sumPixel;
+			}
+
+			Text resultText = new Text(sumr + " " + sumg + " " + sumb);
+			context.write(key, resultText);		
 		}
+	}
+
+	public static String blocks(long k){
+		String result = "";
+
+		int grenzeOben =10;
+		int grenzeUnten=0;
+		for (int i=0; i<=255; i+=10){
+
+			if(k==0)
+				return result = String.valueOf(0);
+
+			else if(k<grenzeOben && k>=grenzeUnten)
+				return result = String.valueOf(i);
+
+			grenzeOben+=10;
+			grenzeUnten+=10;
+		}
+
+		return result;
 	}
 
 	@Override
 	public int run(String[] args) throws Exception {
-		Configuration conf = getConf();
-		FileSystem fs = FileSystem.get(conf);
+		Configuration conf = new Configuration();
+		Path inputPath = new Path (args[0]);
+		Path outputPath = new Path(args[1]);
+		conf.set(ColorAnalyzer.URL, inputPath.toString());
 
 		conf.addResource(new Path("/etc/alternatives/hadoop-conf/core-site.xml"));
 		conf.addResource(new Path("/etc/alternatives/hadoop-conf/hdfs-site.xml"));
 
-		/*
-		Path inputPath = new Path ("/home/cloudera/Pictures/sample_01.jpg");
-		Path outputPath = new Path("picOut1/");
-		Path outputPath2 = new Path("picOut2/");
-		 */
-		Path inputPath = new Path (args[0]);
-		Path outputPath = new Path(args[1]);
+		FileSystem fs = FileSystem.get(conf);
 
 		if (fs.exists(outputPath)) { 
 			fs.delete(outputPath, true);
 		}
 
-		/**
-		 * 
-		FSDataInputStream in = fs.open(inputPath);
-		FSDataOutputStream out = fs.create(outputPath);
-		ImageInputStream imageInput = ImageIO.createImageInputStream(in);
-		BufferedImage image = ImageIO.read(imageInput);
-		 */
-
-
-		Job job = Job.getInstance();
+		Job job = new Job(conf, "color Analyzer");
 		job.setJarByClass(ColorAnalyzer.class);
 
 		job.setMapperClass(ColorAnalyzerMapper.class);
@@ -183,7 +209,6 @@ public class ColorAnalyzer extends Configured implements Tool {
 		FileInputFormat.addInputPath(job, inputPath);	
 		FileOutputFormat.setOutputPath(job, outputPath);
 
-		System.out.println("run finish");
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 
@@ -192,10 +217,7 @@ public class ColorAnalyzer extends Configured implements Tool {
 		if (args.length <2){
 			System.out.println("input and output missing!");
 		}
-		int res = ToolRunner.run(new Configuration(),
-				new ColorAnalyzer(), args);
+		int res = ToolRunner.run(new Configuration(), new ColorAnalyzer(), args);
 		System.exit(res);
-	}
-
-
+	} 
 }
