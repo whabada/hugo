2a35b8ce0d1fdfcae849066fcfd5bec235e735c1
Ben <Lohrengel.Ben@gmail.com>
2016-02-13 02:41:08 -0800

    MapReduce fixed
    Der MapReduceJob laeuft nun. 
    Als Input wird nun eine einzeilige txt Datei erwartet, die einen Link
    zum Bild enthaelt. 
    Die Klasse ist ausfuehrlich kommentiert.

--- src/main/java/de/fhms/abs/ColorAnalyzer.java
+++ src/main/java/de/fhms/abs/ColorAnalyzer.java
@@ -2,7 +2,6 @@ package de.fhms.abs;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -14,7 +13,6 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -23,7 +21,6 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -31,26 +28,20 @@ import org.apache.hadoop.util.ToolRunner;
 public class ColorAnalyzer extends Configured implements Tool {
 
 	private final static DoubleWritable one = new DoubleWritable(1);
-	private final static String URL = "UrlString";
 
 	public static class ColorAnalyzerMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
 
-		private String urlStr;
-		private Configuration conf;
-
-		@Override
-		protected void setup(Context context) {
-			conf = context.getConfiguration();
-			urlStr = conf.get("UrlString");
-		} //TODO Mapper wird haeufiger aufgerufen als er soll
-
 		/**
-		 * Die Mapper Funktion erhaelt ein Bild und analysiert die Farbe saemtlicher Pixel des erhaltenen Images. 
+		 * Die Mapper Funktion erhaelt eine txt Datei, welche eine url zum Bild enthaelt. 
+		 * Es wird die die Farbe saemtlicher Pixel eingeladenen Bildes analysiert, 
+		 * in dem fuer jeden Farbwert gezaehlt wird, wie haeufig die Farbe erscheint. 
 		 * Die Funktion funktioniert nur, wenn das Bild nicht null ist und groeßer als 1x1 ist.
-		 * Für jede Farbe R, G, B wird ausgebenen "RGB Farbcode, 1". 
+		 *  
 		 * @param key Nummer des Frames 
-		 * @param Text url
+		 * @param Text url, link zum Bild. 
 		 * @param context
+		 * @return (Key, one); Für jede Farbe R, G, B wird ausgebenen "RGB Farbcode, one", bspw. R10, one. 
+		 * @return ebenso wird die summe aller Pixel angehanden. 
 		 * @throws IOException
 		 * @throws InterruptedException
 		 */
@@ -58,15 +49,13 @@ public class ColorAnalyzer extends Configured implements Tool {
 
 			BufferedImage image = null;
 			FileSystem fs = FileSystem.get(context.getConfiguration());
-			Path inputPath = new Path(urlStr);
-			try {
-				image = ImageIO.read(new File ("/home/cloudera/Pictures/sample_10.png"));
-			/*	FSDataInputStream in = fs.open(inputPath);
+			Path inputPath = new Path(url.toString());
+			try { //Bild einladen
+				FSDataInputStream in = fs.open(inputPath);
 				ImageInputStream imageInput = ImageIO.createImageInputStream(in);
 				image = ImageIO.read(imageInput); 
-				imageInput.close();*/
 			} catch (IOException e) {
-				System.out.println("no input!");
+				System.out.println(e.getMessage());
 			} 
 
 			if (image != null && image.getWidth() > 1 && image.getHeight() > 1) {
@@ -74,9 +63,6 @@ public class ColorAnalyzer extends Configured implements Tool {
 				int w = image.getWidth();
 				int h = image.getHeight();
 				long sumPixel = w*h;
-				int counter =0;
-				counter ++;
-				System.out.print(counter+ " ");
 
 				int r = 0, g = 0, b = 0;
 				for (int x = 0; x < w; x++) {
@@ -86,19 +72,11 @@ public class ColorAnalyzer extends Configured implements Tool {
 						g = pixel.getGreen(); // gruen
 						b = pixel.getBlue(); // blau
 
-						//TODO RGB Codes falsch!
 						Text RKey =new Text("R "+String.valueOf(r)+ "," + String.valueOf(sumPixel));
 						Text GKey =new Text("G "+String.valueOf(g)+ "," + String.valueOf(sumPixel));
 						Text BKey =new Text("B "+String.valueOf(b)+ "," + String.valueOf(sumPixel));
-						
-				/*		System.out.println("r: " + r);
-						System.out.println("g: " + g);
-						System.out.println("b: " + b); */
 
-
-						/** TODO Blocks kommen spaeter.
-						 * TODO Das mit den Blocks stimmt nicht. 
-						 * Muss der nicht zaehlen, wie haeuig ein Block auftritt? Aber was dann?
+						/** TODO Blocks kommen spaeter. Wie dann?
 						Text RKey =new Text("R"+blocks(sumr)+ "," + String.valueOf(sumPixel));
 						Text GKey =new Text("G"+blocks(sumg)+ "," + String.valueOf(sumPixel));
 						Text BKey =new Text("B"+blocks(sumb)+ "," + String.valueOf(sumPixel));
@@ -114,26 +92,34 @@ public class ColorAnalyzer extends Configured implements Tool {
 			}
 		}
 	}
-//TODO Combiner nicht richtig
+
 	public static class AverageColorCombiner extends Reducer<Text, DoubleWritable, Text, DoubleWritable>{
 
+		/**
+		 * Combiner aggregiert die Haeufigkeit eines "gesehenen" Farbcodes.
+		 * Berechnet anschliessend den relativen Anteil der Farbe am gesamten Farbspektrum
+		 * Berechnet abschliessend den Anteil am Durchschnitt.  
+		 * @param Text key besteht aus dem Farbtyp, den Farbcode und Haeufigkeit, z.B. R 10, 4
+		 * @param DoubleWritable values
+		 * @param Context context
+		 * @return Text: Farbtyp
+		 * @return DoubleWritable: Farbanteil am Durchschnitt
+		 * Gibt als neuen key den Farbcode aus (R G oder B) sowie den Anteil am Durchschnitt
+		 */
 		@Override
 		protected void reduce(Text key, Iterable<DoubleWritable> values,
 				Reducer<Text, DoubleWritable, Text, DoubleWritable>.Context context) throws IOException, InterruptedException {
 
 			String[] splitKey = key.toString().split(",");
-			String code = splitKey[0];
-			long sumPixel =Long.parseLong(splitKey[1]);
-			System.out.println("code: " + code);
-			System.out.println("sumpixel: "+ sumPixel);
+			String code = splitKey[0]; //Farbcode extrahieren, z.B. R10
+			long sumPixel =Long.parseLong(splitKey[1]); //Anzahl der Pixel gesamt
 
 			String[] codeString = code.split(" ");
-			String farbtype = codeString[0];
-			int farbcode = Integer.parseInt(codeString[1]);
-			System.out.println("farbcode: "+ farbcode);
+			String farbtype = codeString[0]; //extrahieren des Farbtypes, R, G oder B 
+			int farbcode = Integer.parseInt(codeString[1]); //extrahieren des Farbcodes, um Beispiel 10.
 
-			double amount = 0;
-
+			double amount = 0; 
+			// Aufsummieren der Haeufigkeit, summiert nach R G und B 
 			if (code.startsWith("R")){ 
 				for (DoubleWritable val : values) {
 					amount += val.get();
@@ -151,14 +137,9 @@ public class ColorAnalyzer extends Configured implements Tool {
 					amount += val.get();
 				}
 			}
-			System.out.println("amount: "+ amount);
 
-
-			double relAnt = amount / sumPixel; //Relativer Anteil 
-			double antDurch = relAnt*farbcode; //Anteil am Durchschnitt
-			System.out.println("relAnt: "+ relAnt);
-			System.out.println("antDurch: "+ antDurch);
-
+			double relAnt = amount / sumPixel; //Berechnung relativer Anteil 
+			double antDurch = relAnt*farbcode; //Berechnung Farbanteil am Durchschnitt
 
 			context.write(new Text(farbtype), new DoubleWritable(antDurch));
 
@@ -167,80 +148,23 @@ public class ColorAnalyzer extends Configured implements Tool {
 
 	public static class AverageColorReducer extends Reducer<Text, DoubleWritable, Text, Text> {
 
-		/** 
-		 * Hier wird gezaehlt, wie häufig ein R G B Code vom Mapper "gesehen" wurde
-		 * Dieses Ergebnis wird aufsummiert und dann als Durchschnittswert ausgegeben 
+		/**
+		 * @param key: Die Eingangskey ist der Farbcode (R,G,B).
+		 * @param Iterable values: enthalten den durchschnittlichen Anteil des Farbcodes 
+		 * @param context
+		 * @return Gibt den finalen Durchschnittswert aller R Werte, aller G Werte und aller B Werte aus
+		 * @return key: R, G oder B 
+		 * @return text: resultValue
 		 */
-
 		@Override
 		protected void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
 
-			//	String[] splitKey = key.toString().split(",");
-			//	String code = splitKey[0];
-			//	long farbcode = Long.parseLong(splitKey[1]);
-			//	String code = key.toString();
-
-			//long sumPixel =0;
-			//	long sumPixel =Long.parseLong(splitKey[2]);
-			//	long farbsum=0; 
-			/*
-			String[] splitResult = key.toString().split(",");
-			String r = splitResult[0];
-			String g = splitResult[1];
-			String b = splitResult[2]; 
-						Text result = new Text (r + " " + g + " "+ b);
-			 */
-
 			int result = 0;
-			
+
 			for (DoubleWritable val : values){
 				result += val.get();
-				System.out.println("result: "+ result);
-
 			}
-			
 			context.write(key, new Text(String.valueOf(result)));
-
-			/*
-			for (IntWritable val : values) {//je nachdem wie haeufig ein Wert angegeben wird, wird dieser addiert
-				sumr += Long.valueOf(splitKey[0]);  
-				sumg += Long.valueOf(splitKey[1]);
-				sumb += Long.valueOf(splitKey[2]); ;
-			} 
-
-			//Durchschnitt bilden
-			sumr = sumr/sumPixel;
-			sumb = sumb/sumPixel;
-			sumg = sumg/sumPixel; */
-
-			/*
-			if (code.startsWith("R")){ 
-//TODO das stimmt nicht! der zaehlt ja die Anzahl, wie haeufig der das gesehen hat, nicht die Anzahl der Farbe
-				for (IntWritable val : values) {
-					farbsum += farbcode; 
-				}
-			}
-
-			if (code.startsWith("G")){
-				for (IntWritable val : values) {
-					farbsum += farbcode;
-				}
-
-			}
-			if (code.startsWith("B")){
-				for (IntWritable val : values) {
-					farbsum += farbcode;
-				}
-			}
-
-			farbsum = farbsum/sumPixel; 
-
-
-		//	Text resultText = new Text(sumr + " " + sumg + " " + sumb);
-			Text resultText = new Text(String.valueOf(farbsum));
-			Text resultKey = new Text (code+splitKey[1]);
-			context.write(resultKey, resultText);	 */
-
 		}
 	}
 	/**
@@ -273,11 +197,8 @@ public class ColorAnalyzer extends Configured implements Tool {
 	@Override
 	public int run(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-	//	Path inputPath = new Path (args[0]);
-	//	Path outputPath = new Path(args[1]);
-		Path inputPath = new Path("sample_10.png");
-		Path outputPath = new Path("pic1Out");
-		conf.set(ColorAnalyzer.URL, inputPath.toString());
+		Path inputPath = new Path (args[0]);
+		Path outputPath = new Path(args[1]);
 
 		conf.addResource(new Path("/etc/alternatives/hadoop-conf/core-site.xml"));
 		conf.addResource(new Path("/etc/alternatives/hadoop-conf/hdfs-site.xml")); 
@@ -288,7 +209,8 @@ public class ColorAnalyzer extends Configured implements Tool {
 			fs.delete(outputPath, true);
 		}
 
-		Job job = new Job(conf, "color Analyzer");
+		Job job = Job.getInstance(conf);
+	//	Job job = new Job(conf, "color Analyzer");
 		job.setJarByClass(ColorAnalyzer.class);
 
 		job.setMapperClass(ColorAnalyzerMapper.class);
@@ -299,8 +221,6 @@ public class ColorAnalyzer extends Configured implements Tool {
 		job.setMapOutputValueClass(DoubleWritable.class); 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
-		
-		//job.setInputFormatClass(FileInputFormat.class);
 
 		FileInputFormat.addInputPath(job, inputPath);	
 		FileOutputFormat.setOutputPath(job, outputPath);
@@ -310,10 +230,10 @@ public class ColorAnalyzer extends Configured implements Tool {
 
 	public static void main(String[] args) throws Exception {
 
-	/*	if (args.length <2){
+		if (args.length <2){
 			System.out.println("input and output missing!");
-		} */
-	
+		} 
+
 		int res = ToolRunner.run(new Configuration(), new ColorAnalyzer(), args);
 		System.exit(res);
 	} 
